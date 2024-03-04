9376d1ec8a7a47f7ba7318840abf8ff566c4e6db
tomruk <41700170+tomruk@users.noreply.github.com>
2024-03-04 22:55:32 +0300

    Add dopm

--- docker.plugin.zsh
+++ docker.plugin.zsh
@@ -1,3 +1,15 @@
 # dos stands for: docker short
 alias dos='docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Image}}"'
 alias dops='docker ps'
+
+dopm() {
+    for id in $(docker ps -a -q); do
+        names=$(docker ps -a --filter "id=$id" --format "{{.Names}}")
+        ports=$(docker port "$id")
+        if [ -z "$ports" ]; then
+            echo "$names"
+        else
+            echo "$names" '---' "$ports"
+        fi
+    done
+}
